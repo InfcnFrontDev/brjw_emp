36b4188b992bbd6df47db04cbada628a141330d0
kecoyo <kecoyo@163.com>
2016-12-13 11:38:38 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+"use strict";
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webpack.loaders');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || "3000";
+
+// global css
+loaders.push({
+	test: /\.css$/,
+	exclude: /[\/\\]src[\/\\]/,
+	loaders: [
+		'style?sourceMap',
+		'css'
+	]
+});
+// local scss modules
+loaders.push({
+	test: /\.scss$/,
+	exclude: /[\/\\](node_modules|bower_components|public\/)[\/\\]/,
+	loaders: [
+		'style?sourceMap',
+		'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+		'postcss',
+		'sass'
+	]
+});
+
+// local css modules
+loaders.push({
+	test: /\.css$/,
+	exclude: /[\/\\](node_modules|bower_components|public\/)[\/\\]/,
+	loaders: [
+		'style?sourceMap',
+		'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+	]
+});
+
+module.exports = {
+	entry: {
+		'index': [
+			'react-hot-loader/patch',
+			'./src/index.jsx'
+		],
+		'admin': [
+			'react-hot-loader/patch',
+			'./src/admin/index.jsx'
+		]
+	},
+	devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
+	output: {
+		publicPath: '/',
+		path: path.join(__dirname, 'public'),
+		filename: 'js/[name].js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders
+	},
+	devServer: {
+		contentBase: "./public",
+		// do not print bundle build stats
+		noInfo: true,
+		// enable HMR
+		hot: true,
+		// embed the webpack-dev-server runtime into the bundle
+		inline: true,
+		// serve index.html in place of 404 responses to allow HTML5 history
+		historyApiFallback: true,
+		port: PORT,
+		host: HOST
+	},
+	plugins: [
+		new webpack.NoErrorsPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/template.html',
+			title: 'Webpack index App',
+			chunks: ['index']
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'admin/index.html',
+			template: './src/template.html',
+			title: 'Webpack admin App',
+			chunks: ['admin']
+		}),
+	]
+};
