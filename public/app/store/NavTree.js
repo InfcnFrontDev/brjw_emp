dbddc7594edc7dba357926b33285a17c3412c8d4
wangpengzsx <15201207203@sina.com>
2016-12-19 09:21:42 +0800

    Merge branch 'master' of https://github.com/InfcnFrontDev/brjw_emp
    # Conflicts:
    #	public/data/menu.json

--- public/app/store/NavTree.js
+++ public/app/store/NavTree.js
@@ -7,8 +7,8 @@ Ext.define('MyUI.store.NavTree', {
 
 	proxy: {
 		type: 'ajax',
-		//url: 'http://192.168.1.166/WebServices/MenuService.ashx?statusCode=ExtTree'
-		url: 'data/menu.json'
+		url: 'http://192.168.1.166/WebServices/MenuService.ashx?statusCode=ExtTree'
+		//url: 'data/menu.json'
 	},
 	root: {
 		text: 'Ext JS',
