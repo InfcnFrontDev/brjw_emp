61c1b0a27173457c60913c94c623e31acf6dd5ad
kecoyo <kecoyo@163.com>
2016-12-19 09:20:12 +0800

    config.js

---
+++ public/js/config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016-12-19.
+ */
+
+var MyUI = MyUI || {};
+
+MyUI.apiPath = 'http://192.168.1.166/';
+
+MyUI.api = {
+	menuTree: MyUI.apiPath + 'WebServices/MenuService.ashx?statusCode=ExtTree'
+	// menuTree: 'data/menu.json'
+};
