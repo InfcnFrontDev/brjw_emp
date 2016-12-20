8f5bb585f42b5bbeed59fe591ff28d8dcc5b5c91
kecoyo <kecoyo@163.com>
2016-12-20 10:22:06 +0800

    app.jsx

--- src/pages/html/app.jsx
+++ src/pages/html/app.jsx
@@ -5,7 +5,6 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-      
         <h1>It Works!</h1>
         <p>This React project just works including <span className={styles.redBg}>module</span> local styles.</p>
         <p>Enjoy!</p>
