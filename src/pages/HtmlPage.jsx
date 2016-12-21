4d44038689df830ce8a148202ef984aaeb43c0f5
wangpengzsx <15201207203@sina.com>
2016-12-21 14:22:37 +0800

    package.json

--- src/pages/HtmlPage.jsx
+++ src/pages/HtmlPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default class HtmlPage extends React.Component {
   render() {
+  	console.log(this.props.abc)
     return (
       <div>
 				<p>Hello HtmlPage!</p>
