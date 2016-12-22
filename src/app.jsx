123de3c96a21ee1b2eacc566ca5e563d64496a1e
guoxm0713 <15930346547@163.com>
2016-12-22 17:04:07 +0800

    1212

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,7 @@ import StagePage from './pages/StagePage'
 import WordPage from './pages/WordPage'
 import ExcelPage from './pages/ExcelPage'
 import HtmlPage from './pages/HtmlPage'
+import TablePage from './pages/TablePage'
 
 export default class App extends React.Component {
 
@@ -45,6 +46,9 @@ export default class App extends React.Component {
 					break;
 				case 'HtmlPage':
 					WebPage = <HtmlPage page={this.state.page}/>;
+					break;
+				case 'TablePage':
+					WebPage = <TablePage page={this.state.page}/>;
 					break;
 				default:
 					WebPage = <HtmlPage page={this.state.page}/>;
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 export default class TablePage extends React.Component {
-  render() {
+	static propTypes = {
+		page: React.PropTypes.object
+	};
+
+	static defaultProps = {
+		page: null
+	};
+
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
+  	render() {
     return (
 			<div>
 				<p>Hello TablePage!</p>
