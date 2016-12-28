f36a5d9046f1e6e1504eb04bd0853c17c6e27cea
guoxm0713 <15930346547@163.com>
2016-12-28 19:01:24 +0800

    222

--- src/pages/heatmap/components/Page.jsx
+++ src/pages/heatmap/components/Page.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
-
+import Echarts from  'echarts'
 export default class Page extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			
+		}
+	}
 
 	render() {
 		let {page} = this.props;
+		let style = {
+			'position': 'absolute',
+			'left': 0,
+			'top': 0,
+			'width':page.Width,
+			'height':page.Height,
+			'border': ' 1px solid #000',
+		};
+
 		return (
-			<div>heatmap</div>
+				<div  id="mypage_ctl00" style={style}></div>
 		)
+
 	}
 
 	componentDidMount() {
+		this.initPage();
+	}
+
+	initPage() {
+		
+		let {page} = this.props;
+		var myChart=Echarts.init(document.getElementById('mypage_ctl00'));
+		 	Echarts.registerMap('beijing', page.JsonData);
+		
+	   	var m=BuildChartOption('heatmap',{ 'file': page.JsonData, 'name': page.JsonData});
+	   myChart.setOption(m)
 	}
 }
