0a9a24c9cef76e22ee2b4d8462a64cd2504e9837
guoxm0713 <15930346547@163.com>
2016-12-29 17:29:00 +0800

    ww

---
+++ src/pages/energybalance/EnergyBalancePage.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+export default class EnergyBalancePage extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			
+		}
+	}
+
+	render() {
+		let {page} = this.props;
+		let style = {
+			'width':page.Size.Width,
+			'height':page.Size.Height,
+			'position':'absolute', 
+			'left': '0px',
+			'top': '0px', 
+			'border': '1px solid ',
+			'background':'url(../images/954586bb-bb08-4b8b-bb96-ae82e83f8fa0.png)'
+			 
+		};
+		let canvas_style={};
+		canvas_style = {
+				width: page.Size.Width,
+				height: page.Size.Height
+			};
+		let height =page.Size.Height;
+		let width =	page.Size.Width;
+		return (
+			<div id="mypage_ctl00" style={style}>
+				<canvas id="canvas" {...canvas_style}></canvas>
+			</div>
+			
+		)
+
+	}
+
+	componentDidMount() {
+		this.initPage();
+	}
+
+	initPage() {
+		let me = this;
+
+		var Canvas = new fabric.Canvas('canvas', {
+			selection: false,
+			allowTouchScrolling: true,
+			renderOnAddRemove: false
+		});
+	}
+
+}
